4be66ac28978119acec076f9d709b10856fb2bc1
Patrick <24683548+pohuing@users.noreply.github.com>
2022-09-15 21:23:54 +0200

    Introduced TaskCategories and a nested listing

---
+++ lib/model/task_category.dart
@@ -0,0 +1,33 @@
+import 'package:lernapp/model/task.dart';
+import 'package:uuid/uuid.dart';
+
+class TaskCategory {
+  List<TaskCategory>? subCategories;
+  List<Task>? tasks;
+  String title;
+
+  TaskCategory({required this.title, this.subCategories, this.tasks});
+
+  int get numberOfChildren {
+    return (subCategories?.length ?? 0) + (tasks?.length ?? 0);
+  }
+
+  /// Recursively search if task in tree
+  Task? findTask(UuidValue uuid) {
+    Task? task;
+    for (var t in tasks ?? <Task>[]) {
+      if (t.uuid == uuid) {
+        return t;
+      }
+    }
+    if (task == null) {
+      for (var c in subCategories ?? <TaskCategory>[]) {
+        var t = c.findTask(uuid);
+        if (t != null) {
+          return t;
+        }
+      }
+    }
+    return null;
+  }
+}
