82ef6ec24cdb28ceb3f1883cbb10ef0cc7322d58
Patrick <24683548+pohuing@users.noreply.github.com>
2022-09-28 00:46:20 +0200

    Add option to select bright and dark theme colours

--- lib/widgets/general_purpose/color_selection/color_picker_dialogue.dart
+++ lib/widgets/general_purpose/color_selection/color_picker_dialogue.dart
@@ -13,16 +13,22 @@ class ColorPickerDialogue extends StatefulWidget {
 }
 
 class _ColorPickerDialogueState extends State<ColorPickerDialogue> {
-  Color color = Colors.red;
+  Color darkThemeColor = Colors.red[400]!;
+  Color brightThemeColor = Colors.red[900]!;
+  bool dualColours = false;
 
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
       scrollable: true,
+      title: const Text('Add a new Color'),
       actions: [
         ElevatedButton(
           onPressed: () {
-            widget.colorController.colors.add(ColorPair(color, color));
+            widget.colorController.colors.add(
+              ColorPair(darkThemeColor,
+                  !dualColours ? darkThemeColor : brightThemeColor),
+            );
             Navigator.of(context, rootNavigator: false).pop(true);
           },
           child: const Text('Confirm'),
@@ -31,17 +37,77 @@ class _ColorPickerDialogueState extends State<ColorPickerDialogue> {
       content: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          ColorPicker(
-            portraitOnly: true,
-            pickerColor: color,
-            onColorChanged: (Color value) {
-              setState(() {
-                color = value;
-              });
-            },
-          )
+          SwitchListTile(
+            title: const Text('Use separate colors'),
+            value: dualColours,
+            onChanged: (value) => setState(() => dualColours = value),
+          ),
+          _AdaptiveRowColumn(
+            children: [
+              SizedBox(
+                width: 300,
+                height: 488,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    if (dualColours)
+                      Text(
+                        'Bright Theme Colour',
+                        style: Theme.of(context).textTheme.titleMedium,
+                      ),
+                    ColorPicker(
+                      portraitOnly: true,
+                      pickerColor: darkThemeColor,
+                      onColorChanged: (Color value) {
+                        setState(() {
+                          darkThemeColor = value;
+                        });
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              if (dualColours)
+                SizedBox(
+                  width: 300,
+                  height: 488,
+                  child: Column(
+                    children: [
+                      Text(
+                        'Dark Theme Colour',
+                        style: Theme.of(context).textTheme.titleMedium,
+                      ),
+                      ColorPicker(
+                        portraitOnly: true,
+                        pickerColor: brightThemeColor,
+                        onColorChanged: (Color value) {
+                          setState(() {
+                            brightThemeColor = value;
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                )
+            ],
+          ),
         ],
       ),
     );
+  }
+}
+
+class _AdaptiveRowColumn extends StatelessWidget {
+  final List<Widget> children;
+
+  const _AdaptiveRowColumn({required this.children});
+
+  @override
+  Widget build(BuildContext context) {
+    if (MediaQuery.of(context).size.width > 750) {
+      return Row(children: children);
+    } else {
+      return Column(children: children);
+    }
   }
 }
