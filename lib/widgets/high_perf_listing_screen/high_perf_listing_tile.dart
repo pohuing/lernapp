e03832347208364852f1da98669f16d8f92f9886
Patrick <24683548+pohuing@users.noreply.github.com>
2022-10-22 04:51:34 +0200

    Add category selection Add expanded color

--- lib/widgets/high_perf_listing_screen/high_perf_listing_tile.dart
+++ lib/widgets/high_perf_listing_screen/high_perf_listing_tile.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:lernapp/model/high_performance_listing_wrappers.dart';
+
+import '../../blocs/selection_cubit.dart';
 
 class HighPerfListingTile extends StatelessWidget {
   final ListingEntryCategory entry;
@@ -12,21 +15,39 @@ class HighPerfListingTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.only(left: entry.depth * 16),
-      child: ListTile(
-        onTap: () {
-          onTap?.call();
-        },
-        trailing: IgnorePointer(
-          child: ExpandIcon(
-            onPressed: (_) => null,
-            isExpanded: entry.isExpanded,
+      child: BlocBuilder<SelectionCubit, SelectionState>(
+        builder: (context, state) => ListTile(
+          onTap: () {
+            onTap?.call();
+          },
+          leading: leading(context, state),
+          trailing: IgnorePointer(
+            child: ExpandIcon(
+              onPressed: (_) => null,
+              isExpanded: entry.isExpanded,
+            ),
+          ),
+          title: Text(
+            entry.category.title,
+            style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                  color: entry.isExpanded
+                      ? Theme.of(context).colorScheme.primary
+                      : null,
+                ),
           ),
         ),
-        title: Text(
-          entry.category.title,
-          style: Theme.of(context).textTheme.titleMedium!,
-        ),
       ),
+    );
+  }
+
+  Widget? leading(BuildContext context, SelectionState state) {
+    if (!state.isSelecting || entry.category.numberOfChildren == 0) {
+      return null;
+    }
+    return Checkbox(
+      value: state.entireCategoryIsSelected(entry.category),
+      onChanged: (value) =>
+          context.read<SelectionCubit>().toggleCategory(entry.category),
     );
   }
 }
