07a5f7f28a0102f9bb20d37b1bb0b7a159d0a7d0
Patrick <24683548+pohuing@users.noreply.github.com>
2023-02-22 11:52:10 +0100

    Minor change to trigger CI

--- lib/widgets/task_screen/hint_card.dart
+++ lib/widgets/task_screen/hint_card.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-import '../general_purpose/scrollable_selectable_text.dart';
-
 class HintCard extends StatelessWidget {
   final String hint;
 
@@ -14,7 +12,7 @@ class HintCard extends StatelessWidget {
         padding: const EdgeInsets.all(8.0),
         child: Row(
           children: [
-            ScrollableSelectableText(text: hint),
+            Text(hint),
             const VerticalDivider(width: 8),
             IgnorePointer(
               child: IconButton(
