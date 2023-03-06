d03c841e94f015721bcb4b0e14649b066e7fd130
Patrick <24683548+pohuing@users.noreply.github.com>
2023-03-06 19:46:32 +0100

    Fix tests

--- test/integration_test/performance_test.dart
+++ test/integration_test/performance_test.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:hive/hive.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:lernapp/blocs/preferences/preferences_bloc.dart';
+import 'package:lernapp/model/preferences/repository_configuration/repository_settings.dart';
+import 'package:lernapp/model/preferences/theme_preferences.dart';
+import 'package:lernapp/repositories/preferences_repository.dart';
 import 'package:lernapp/widgets/drawing_area/drawing_area.dart';
 import 'package:lernapp/widgets/general_purpose/scratchpad.dart';
 import 'package:provider/provider.dart';
@@ -11,24 +15,30 @@ void main() {
   final binding = IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   testWidgets('Draw example', (widgetTester) async {
+    Hive.init('.');
+    final prefsRepo =
+        PreferencesRepository(await Hive.openBox('testing_performance_test'));
     final prefs = PreferencesBloc(
       PreferencesStateBase(
         RepositorySettings([], null),
         ThemePreferences.defaults(),
       ),
+      prefsRepo,
     );
 
     var drawingArea = MaterialApp(
-      builder: (context, child) => MultiProvider(
+      home: MultiProvider(
         providers: [
           BlocProvider.value(value: prefs),
         ],
         child: Scaffold(
-          body: Stack(
-            children: [
-              const Scratchpad(),
-              PerformanceOverlay.allEnabled(),
-            ],
+          body: Builder(
+            builder: (context) => Stack(
+              children: [
+                const Scratchpad(),
+                PerformanceOverlay.allEnabled(),
+              ],
+            ),
           ),
         ),
       ),
