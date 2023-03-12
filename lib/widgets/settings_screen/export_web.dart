7db61973dd54d795bdaad4639f62c8cdfdb5737b
Patrick <24683548+pohuing@users.noreply.github.com>
2023-03-12 16:38:24 +0100

    Fix missing feature in export web causing compilation to fail

--- lib/widgets/settings_screen/export_web.dart
+++ lib/widgets/settings_screen/export_web.dart
@@ -5,11 +5,22 @@ import 'dart:html' show AnchorElement;
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:intl/intl.dart';
 import 'package:lernapp/blocs/tasks/tasks_bloc.dart';
 import 'package:lernapp/repositories/task_repository.dart';
 
-Future<void> export(BuildContext context, String filename) async {
+Future<void> export(
+  BuildContext context,
+  String filename, {
+  bool appendTimestamp = false,
+}) async {
   final repo = context.read<TasksBloc>().repository;
+  if (appendTimestamp) {
+    final now = DateTime.now();
+    filename =
+        '$filename ${DateFormat(DateFormat.YEAR_MONTH_DAY).format(now)} ${DateFormat(DateFormat.HOUR24_MINUTE_SECOND).format(now)}';
+  }
+
   if (repo is! HiveTaskRepository) {
     return;
   }
